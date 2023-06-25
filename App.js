dd7ffcee9a18a438745b99bfb4c85988c17bbb01
Josh Shavers <shaversj@gmail.com>
2023-06-25 13:29:25 -0500

    Initial Commit

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
+import {useFonts} from "expo-font";
+import {NativeWindStyleSheet} from "nativewind";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const staticImage = require("./assets/images/image-qr-code.png")
+const customFonts = {
+  'Outfit Thin': require("./assets/fonts/Outfit-VariableFont_wght.ttf")
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+NativeWindStyleSheet.setOutput({
+  default: "native",
 });
+export default function App() {
+    const [loaded] = useFonts(customFonts);
+    if (!loaded) {
+        return null;
+    }
+    return (
+        <View className="container flex-1  items-center justify-center min-h-screen bg-black">
+            <View className="w-[322px] bg-white flex items-center rounded-2xl">
+                <Image source={staticImage} className="object-fill w-[287px] h-[287px] m-4 rounded-xl"></Image>
+                <Text className="text-xl leading-[26px] text-center px-5 font-Outfit Thin font-extrabold tracking-wide text-dark-blue">Improve your front-end skills by building projects</Text>
+                <Text className="pt-4 pb-12 text-[15px] px-6 leading-5 text-center font-Outfit Thin text-grayish-blue">Scan the QR code to visit Frontend Mentor and take your coding
+                    skills to the next level</Text>
+            </View>
+        </View>
+    );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "grayish-blue": "hsl(220, 15%, 55%)",
+        "dark-blue": "hsl(218, 44%, 22%)",
+      },
+      fontFamily: {
+        'Outfit Thin': ["Outfit", "sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
